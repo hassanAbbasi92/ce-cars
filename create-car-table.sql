b6edc5b4d7145b78a4ea5377aab3970bbcf08cb3
hassan.abbasi <hassan.abbasi@autoshpere.ai>
2024-08-02 14:50:47 +0500

    Added create table script

---
+++ create-car-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `car` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `length` double DEFAULT NULL,
+  `weight` double DEFAULT NULL,
+  `velocity` double DEFAULT NULL,
+  `color` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
